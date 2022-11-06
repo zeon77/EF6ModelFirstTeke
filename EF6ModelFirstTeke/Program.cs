a670bef782f3ba3c9e18c51a8bbfdd62d85e0f08
zeon77 <2716313+zeon77@users.noreply.github.com>
2022-11-06 23:15:09 +0100

    4. feladat

--- EF6ModelFirstTeke/Program.cs
+++ EF6ModelFirstTeke/Program.cs
@@ -12,10 +12,23 @@ namespace EF6ModelFirstTeke
         {
             using (var db = new TekeDbModelContainer())
             {
-                //3. feladat
+                /* 3. feladat
+                 * Listázza ki az A korcsoportban indulók névsorát ábécé rendben!
+                 */
                 Console.WriteLine("3. feladat: Az A korcsoportban indulók névsora ábécé rendben:");
                 db.Versenyzok.Where(v => v.Korcsop == "A").OrderBy(v => v.Nev).ToList().ForEach(v => Console.WriteLine(v.Nev));
 
+                /* 4. feladat:
+                 * Listázza ki azon versenyzők rajtszámait, akiknek volt üres gurítása! Ha több üres gurítása
+                 * volt valakinek, akkor is csak egyszer írja ki a rajtszámát!
+                 */
+                Console.WriteLine("\n4. feladat: Üres gurítással rendelkező versenyzők:");
+                (from v in db.Versenyzok
+                join e in db.Eredmenyek
+                on v.Rajtszam equals e.VersenyzoRajtszam
+                where e.Ures > 0
+                select new { Rajtszám = v.Rajtszam, Név = v.Nev }).Distinct().ToList().ForEach(v => Console.WriteLine($"{v.Rajtszám}, {v.Név}"));
+                
             }
             Console.ReadKey();
         }
