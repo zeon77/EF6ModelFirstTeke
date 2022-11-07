fce276e5223082b0fa0c6f8ae099b02143786cd6
zeon77 <2716313+zeon77@users.noreply.github.com>
2022-11-07 01:00:36 +0100

    5. feladat

--- EF6ModelFirstTeke/Program.cs
+++ EF6ModelFirstTeke/Program.cs
@@ -28,7 +28,42 @@ namespace EF6ModelFirstTeke
                 on v.Rajtszam equals e.VersenyzoRajtszam
                 where e.Ures > 0
                 select new { Rajtszám = v.Rajtszam, Név = v.Nev }).Distinct().ToList().ForEach(v => Console.WriteLine($"{v.Rajtszám}, {v.Név}"));
-                
+
+                /* 5. feladat:
+                 * Listázza ki minden versenyzőre az átlagos tarolási pontértékét! A versenyzők neve mellett
+                 * a számított mező címkéje „atlagpont” legyen! A listát rendezze a számított mező szerint
+                 * csökkenő rendbe! 5. feladat:)
+                 */
+                Console.WriteLine("\n5. feladat (a):");
+                Console.WriteLine("Név, Átlagpont");
+                (from v in db.Versenyzok
+                 join e in db.Eredmenyek
+                 on v.Rajtszam equals e.VersenyzoRajtszam
+                 group new { v.Nev, e.Tarolas } by v.Rajtszam into tarolasGroup
+                 let avg = tarolasGroup.Average(x => x.Tarolas)
+                 orderby avg descending
+                 select new { Név = tarolasGroup.FirstOrDefault().Nev, Átlag = avg } //itt is azvan mint a (b) verzióban
+                 ).ToList().ForEach(r => Console.WriteLine($"{r.Név}, {r.Átlag}"));
+
+                //félig query félig metoh syntax
+                Console.WriteLine("\n5. feladat (b):");
+                Console.WriteLine("Név, Átlagpont");
+                (from v in db.Versenyzok
+                 join e in db.Eredmenyek
+                 on v.Rajtszam equals e.VersenyzoRajtszam
+                 group new { v.Nev, e.Tarolas } by v.Rajtszam) // itt célszerű rajtszám szerint csoportosítani, mert lehetnek azonos nevű versenyzők...
+                 .Select(g => new { Név = g.FirstOrDefault().Nev, Átlag = g.Average(x => x.Tarolas) }) //itt viszont akkor nem jó a g.Key, mert az a rajtszám lesz, ki kell venni az első elemet...
+                 .OrderByDescending(x => x.Átlag).ToList()
+                 .ForEach(r => Console.WriteLine($"{r.Név}, {r.Átlag}"));
+
+                //Opps. Ha már van Navigation Property... a join-ra és a group by-ra sincs szükség!!!
+                Console.WriteLine("\n5. feladat (c):");
+                Console.WriteLine("Név, Átlagpont");
+                db.Versenyzok
+                    .Select(v => new { Név = v.Nev, Átlag = v.Eredmenyek.Average(e => e.Tarolas) })
+                    .OrderByDescending(x => x.Átlag).ToList()
+                    .ForEach(x => Console.WriteLine($"{x.Név}, {x.Átlag}"));
+
             }
             Console.ReadKey();
         }
