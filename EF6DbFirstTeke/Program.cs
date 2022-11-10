0ed95c53a8d67f2efa410904f58cd9a413992df8
zeon77 <2716313+zeon77@users.noreply.github.com>
2022-11-10 03:00:13 +0100

    Project EF6DbFirstTeke added.

---
+++ EF6DbFirstTeke/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6DbFirstTeke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new TekeDbEntities())
+            {
+                db.Egyesuleteks.ToList()[1].Versenyzoks.ToList().ForEach(v => Console.WriteLine(v.Nev));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
